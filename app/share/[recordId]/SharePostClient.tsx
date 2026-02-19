57391477ab149babcb8cca3c516780cdcafe1a25
矢内　晋（Yanai Shin／Wing-AI Lab) <s-yanai@hm-hst.co.jp>
2026-02-19 22:07:26 +0900

    useEffect, useState は 1回だけ import

--- app/share/[recordId]/SharePostClient.tsx
+++ app/share/[recordId]/SharePostClient.tsx
@@ -2,8 +2,6 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-
-import { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
 
